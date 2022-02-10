cd659dcce54d4c48e3fe7a7b3365ca16aad31bfe
ccaron01 <79853139+ccaron01@users.noreply.github.com>
2022-02-09 22:34:57 -0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-print('welcome to the band name generator!')
-
-Name_city = (input("whats the name of the city you grow up?\n"))
-Name_pet = (input("whats yours pets name?\n"))
-
-print( Name_city + ' ' + Name_pet)
